4e93451f223be3f5d910476875a27f3163355d9a
Jagan-97 <jagan.psaran38@gmail.com>
2022-10-13 06:34:51 +0530

    Added edit person logic

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -12,17 +12,20 @@ public class AddressBookMain {
         while(!isExit){
             System.out.println("Select Option" +
                     "\n1.Add person" +
-                    "\n2.show Address Book" +
-                    "\n3.Exit");
+                    "\n2.Edit person" +
+                    "\n3.show Address Book" +
+                    "\n4.Exit");
             int choice = Integer.parseInt(scanner.nextLine());
             switch (choice) {
                 case 1:
                     addressBookMain.addPerson();
                     break;
                 case 2:
+                    addressBookMain.editPerson();
+                case 3:
                     addressBookMain.showAddressBook();
                     break;
-                case 3:
+                case 4:
                     isExit = true;
                     break;
                 default:
@@ -50,6 +53,29 @@ public class AddressBookMain {
         addressBook.add(person);
     }
 
+    private void editPerson(){
+        System.out.println("Enter name: ");
+        String name = scanner.nextLine();
+        Person person = null;
+        for (int i=0; i < addressBook.size();i++) {
+            if(name.equals(addressBook.get(i).getFirstName() )
+                    || name.equals(addressBook.get(i).getLastName())){
+                person = addressBook.get(i);
+                break;
+            }
+        }
+        if (person!= null) {
+            System.out.println("Enter City");
+            String city = scanner.nextLine();
+            System.out.println("Enter Mobile");
+            long mobileNumber = Long.parseLong(scanner.nextLine());
+            person.setCity(city);
+            person.setMobileNumber(mobileNumber);
+
+        } else {
+            System.out.println("No Contact found");
+        }
+    }
     private void showAddressBook(){
         for(int i=0; i <addressBook.size(); i++){
             System.out.println(addressBook.get(i));
